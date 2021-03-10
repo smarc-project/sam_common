3d1cc0944695e5266abeee5e86dbc6461c6f4082
Ozer <ozero@kth.se>
2021-03-10 09:16:33 +0100

    removed continous 0-publishing, publishing at 10hz

--- sam_joystick/scripts/joystick_to_sam_setpoints.py
+++ sam_joystick/scripts/joystick_to_sam_setpoints.py
@@ -18,31 +18,47 @@ class JoystickTranslator(object):
         self.thruster2_pub = rospy.Publisher("core/thruster2_cmd", ThrusterRPM, queue_size=1)
         self.vector_pub = rospy.Publisher("core/thrust_vector_cmd", ThrusterAngles, queue_size=1)
 
+        self.rpm_msg = ThrusterRPM()
+        self.vec_msg = ThrusterAngles()
+
+        self.published_zero_rpm_once = False
+        self.published_zero_vec_once = False
+
     def rpm_cb(self, twist):
-        rospy.loginfo("xy vector:{}".format(twist))
         # linear.x is forward, straight to RPMs
-        rpm = int(twist.linear.x)
-        rpm_msg = ThrusterRPM()
-        rpm_msg.rpm = rpm
-
-        self.thruster1_pub.publish(rpm_msg)
-        self.thruster2_pub.publish(rpm_msg)
-
-
+        self.rpm_msg.rpm = int(twist.linear.x)
 
     def vector_cb(self, twist):
-        rospy.loginfo("z vector:{}".format(twist))
-
         # angular.z is horizontal angle
         # angular.y is vertical angle
         hori = math.radians(twist.angular.z)
         vert = math.radians(twist.angular.y)
 
-        vec_msg = ThrusterAngles()
-        vec_msg.thruster_horizontal_radians = hori
-        vec_msg.thruster_vertical_radians = vert
+        self.vec_msg.thruster_horizontal_radians = hori
+        self.vec_msg.thruster_vertical_radians = vert
 
-        self.vector_pub.publish(vec_msg)
+
+    def publish(self):
+        if self.rpm_msg.rpm != 0 or not self.published_zero_rpm_once:
+            self.thruster1_pub.publish(self.rpm_msg)
+            self.thruster2_pub.publish(self.rpm_msg)
+            rospy.loginfo("Rpm:{}".format(self.rpm_msg.rpm))
+
+            self.published_zero_rpm_once = self.rpm_msg.rpm == 0
+
+        if self.vec_msg.thruster_horizontal_radians != 0 or \
+           self.vec_msg.thruster_vertical_radians != 0 or \
+           not self.published_zero_vec_once:
+            self.vector_pub.publish(self.vec_msg)
+            rospy.loginfo("Vec:{},{}".format(self.vec_msg.thruster_horizontal_radians, self.vec_msg.thruster_vertical_radians))
+
+            self.published_zero_vec_once = self.vec_msg.thruster_horizontal_radians == 0 and\
+                                           self.vec_msg.thruster_vertical_radians == 0
+
+        # reset the stuff
+        self.rpm_msg.rpm = 0
+        self.vec_msg.thruster_horizontal_radians = 0
+        self.vec_msg.thruster_vertical_radians = 0
 
 
 
@@ -51,7 +67,12 @@ if __name__ == "__main__":
     rospy.init_node("joystick_to_sam_setpoints")
     jt = JoystickTranslator()
     rospy.loginfo("Joystick translator for SAM is running...")
-    rospy.spin()
+
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        jt.publish()
+        rate.sleep()
+
 
 
 

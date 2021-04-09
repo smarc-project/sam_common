e6612484c6c1ad2de1797fb0be38ad7bfa922446
Ozer Ozkahraman <ozero@kth.se>
2021-04-09 11:04:58 +0200

    Changed topics for rpm control

--- sam_joystick/scripts/joystick_to_sam_setpoints.py
+++ sam_joystick/scripts/joystick_to_sam_setpoints.py
@@ -5,8 +5,7 @@
 import rospy
 import math
 
-from sam_msgs.msg import ThrusterAngles
-from smarc_msgs.msg import ThrusterRPM
+from sam_msgs.msg import ThrusterAngles, ThrusterRPMs
 from geometry_msgs.msg import Twist
 
 class JoystickTranslator(object):
@@ -14,11 +13,12 @@ class JoystickTranslator(object):
         self.rpm_sub = rospy.Subscriber("ctrl/rpm_joystick", Twist, self.rpm_cb, queue_size=1)
         self.vector_sub = rospy.Subscriber("ctrl/vector_deg_joystick", Twist, self.vector_cb, queue_size=1)
 
-        self.thruster1_pub = rospy.Publisher("core/thruster1_cmd", ThrusterRPM, queue_size=1)
-        self.thruster2_pub = rospy.Publisher("core/thruster2_cmd", ThrusterRPM, queue_size=1)
+        #self.thruster1_pub = rospy.Publisher("core/thruster1_cmd", ThrusterRPM, queue_size=1)
+        #self.thruster2_pub = rospy.Publisher("core/thruster2_cmd", ThrusterRPM, queue_size=1)
+        self.thruster_pub = rospy.Publisher("core/rpm_cmd", ThrusterRPMs, queue_size=1)
         self.vector_pub = rospy.Publisher("core/thrust_vector_cmd", ThrusterAngles, queue_size=1)
 
-        self.rpm_msg = ThrusterRPM()
+        self.rpm_msg = ThrusterRPMs()
         self.vec_msg = ThrusterAngles()
 
         self.published_zero_rpm_once = False
@@ -26,7 +26,9 @@ class JoystickTranslator(object):
 
     def rpm_cb(self, twist):
         # linear.x is forward, straight to RPMs
-        self.rpm_msg.rpm = int(twist.linear.x)
+        #self.rpm_msg.rpm = int(twist.linear.x)
+        self.rpm_msg.thruster_1_rpm = int(twist.linear.x)
+        self.rpm_msg.thruster_2_rpm = int(twist.linear.x)
 
     def vector_cb(self, twist):
         # angular.z is horizontal angle
@@ -39,12 +41,13 @@ class JoystickTranslator(object):
 
 
     def publish(self):
-        if self.rpm_msg.rpm != 0 or not self.published_zero_rpm_once:
-            self.thruster1_pub.publish(self.rpm_msg)
-            self.thruster2_pub.publish(self.rpm_msg)
-            rospy.loginfo("Rpm:{}".format(self.rpm_msg.rpm))
+        if self.rpm_msg.thruster_1_rpm != 0 or not self.published_zero_rpm_once:
+            #self.thruster1_pub.publish(self.rpm_msg)
+            #self.thruster2_pub.publish(self.rpm_msg)
+            self.thruster_pub.publish(self.rpm_msg)
+            rospy.loginfo("Rpm:{}".format(self.rpm_msg.thruster_1_rpm))
 
-            self.published_zero_rpm_once = self.rpm_msg.rpm == 0
+            self.published_zero_rpm_once = self.rpm_msg.thruster_1_rpm == 0
 
         if self.vec_msg.thruster_horizontal_radians != 0 or \
            self.vec_msg.thruster_vertical_radians != 0 or \
@@ -56,7 +59,9 @@ class JoystickTranslator(object):
                                            self.vec_msg.thruster_vertical_radians == 0
 
         # reset the stuff
-        self.rpm_msg.rpm = 0
+        #self.rpm_msg.rpm = 0
+        self.rpm_msg.thruster_1_rpm = 0
+        self.rpm_msg.thruster_2_rpm = 0
         self.vec_msg.thruster_horizontal_radians = 0
         self.vec_msg.thruster_vertical_radians = 0
 
@@ -68,7 +73,7 @@ if __name__ == "__main__":
     jt = JoystickTranslator()
     rospy.loginfo("Joystick translator for SAM is running...")
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(12)
     while not rospy.is_shutdown():
         jt.publish()
         rate.sleep()
